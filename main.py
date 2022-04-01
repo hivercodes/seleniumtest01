b281ccaa2ae4746472737e500f4055c0fae01d6e
Lars <lars.eriksson.mail@gmail.com>
2022-04-01 19:37:10 +0200

    Kinda working? Still have a bug, but done for today.

--- main.py
+++ main.py
@@ -2,11 +2,41 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import time
 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver.get("https://www.amazon.com/UGREEN-Coupler-Ethernet-Extender-Connector/dp/B016B13U9Y/ref=sr_1_3?crid=1KU8LQFN71VHU&keywords=ethernet+extender&qid=1648573618&sprefix=%2Caps%2C145&sr=8-3")
-price = driver.find_element(By.XPATH, '//*[@id="corePrice_feature_div"]/div/span/span[2]')
-print(price.text)
-driver.close()
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
+
+while True:
+    cookie = driver.find_element(By.ID, "cookie")
+    cookie.click()
+    money = int(driver.find_element(By.ID, "money").text)
+    store = driver.find_element(By.ID, "store").text
+    storedata = store.split("\n")
+    things = []
+    price = []
+    for i in range(len(storedata)):
+        if i % 2 == 0:
+            things.append(storedata[i])
+
+    for l in things:
+        split_tp_price = l.split(" ")[-1]
+        price.append(int(split_tp_price.replace(",", "")))
+        print(split_tp_price)
+
+    store_options = {
+        price[7]: '//*[@id="buyCursor"]',
+        price[6]: '//*[@id="buyGrandma"]',
+        price[5]: '//*[@id="buyFactory"]',
+        price[4]: '//*[@id="buyMine"]',
+        price[3]: '//*[@id="buyShipment"]',
+        price[2]: '//*[@id="buyAlchemy lab"]',
+        price[1]: '//*[@id="buyPortal"]',
+        price[0]: '//*[@id="buyTime machine"]'
+    }
+    for key in store_options:
+        time.sleep(0.1)
+        buy = driver.find_element(By.XPATH, store_options[key])
+        buy.click()
