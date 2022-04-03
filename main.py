e8cc604576a488bbaf86b41f68b0e92d1eb2f985
Lars <lars.eriksson.mail@gmail.com>
2022-04-03 16:32:22 +0200

    Changed the store buying to work. Also deleted a large amount that could be done better and easier with just a list.

--- main.py
+++ main.py
@@ -11,32 +11,22 @@ driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 while True:
     cookie = driver.find_element(By.ID, "cookie")
-    cookie.click()
-    money = int(driver.find_element(By.ID, "money").text)
-    store = driver.find_element(By.ID, "store").text
-    storedata = store.split("\n")
-    things = []
-    price = []
-    for i in range(len(storedata)):
-        if i % 2 == 0:
-            things.append(storedata[i])
+    clicks = 0
+    while clicks < 500:
+        cookie.click()
+        clicks = clicks + 1
+    clicks = 0
 
-    for l in things:
-        split_tp_price = l.split(" ")[-1]
-        price.append(int(split_tp_price.replace(",", "")))
-        print(split_tp_price)
+    store_list = ['//*[@id="buyTime machine"]',
+                  '//*[@id="buyPortal"]',
+                  '//*[@id="buyAlchemy lab"]',
+                  '//*[@id="buyShipment"]', '//*[@id="buyMine"]',
+                  '//*[@id="buyFactory"]',
+                  '//*[@id="buyGrandma"]',
+                  '//*[@id="buyCursor"]'
+                  ]
 
-    store_options = {
-        price[7]: '//*[@id="buyCursor"]',
-        price[6]: '//*[@id="buyGrandma"]',
-        price[5]: '//*[@id="buyFactory"]',
-        price[4]: '//*[@id="buyMine"]',
-        price[3]: '//*[@id="buyShipment"]',
-        price[2]: '//*[@id="buyAlchemy lab"]',
-        price[1]: '//*[@id="buyPortal"]',
-        price[0]: '//*[@id="buyTime machine"]'
-    }
-    for key in store_options:
+    for key in range(len(store_list)):
         time.sleep(0.1)
-        buy = driver.find_element(By.XPATH, store_options[key])
+        buy = driver.find_element(By.XPATH, store_list[key])
         buy.click()
